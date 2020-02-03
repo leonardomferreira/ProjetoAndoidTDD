7c1443e47f6e24f8d72ce1c8fd59bff8cb9c6599
leo <leonardoferreira54@outlook.com.br>
2020-02-03 15:57:00 -0300

    Primeiro commit

--- src/main/java/br/com/rsinet/hub_tdd/numeric/NumericPhone.java
+++ src/main/java/br/com/rsinet/hub_tdd/numeric/NumericPhone.java
@@ -1,10 +1,13 @@
 package br.com.rsinet.hub_tdd.numeric;
 
 
+import java.time.Duration;
+
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 
 
--- src/main/java/br/com/rsinet/hub_tdd/pageObject/Home_Page.java
+++ src/main/java/br/com/rsinet/hub_tdd/pageObject/Home_Page.java
@@ -7,22 +7,34 @@ public class Home_Page {
 
 	private static AndroidDriver driver;
 	
+	public static MobileElement lnk_Login2(AndroidDriver driver) {
+		return (MobileElement) driver.findElementByXPath("//hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout");
+	}
+
+	public static MobileElement lnk_Home(AndroidDriver driver) {
+		return (MobileElement) driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"Home Page\"]");
+	}
+
 	public static MobileElement lnk_Menu(AndroidDriver driver) {
-		
+
 		return (MobileElement) driver.findElementById("com.Advantage.aShopping:id/imageViewMenu");
 	}
+
 	public static MobileElement lnk_Login(AndroidDriver driver) {
 		return (MobileElement) driver.findElementById("com.Advantage.aShopping:id/linearLayoutLogin");
 	}
+
 	public static MobileElement clk_CreateAccount(AndroidDriver driver) {
 		return (MobileElement) driver.findElementById("com.Advantage.aShopping:id/textViewSingUpToday");
- }
+	}
+
 	public static MobileElement clk_Pesquisa(AndroidDriver driver) {
 		return (MobileElement) driver.findElementById("com.Advantage.aShopping:id/editTextSearch");
 
- }
+	}
+
 	public static MobileElement clk_Lupa(AndroidDriver driver) {
 		return (MobileElement) driver.findElementById("com.Advantage.aShopping:id/imageViewSearch");
 
-}
+	}
 }
