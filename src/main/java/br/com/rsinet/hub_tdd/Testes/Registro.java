9d0f43558463de509a109cd97b9f77b54c8ae90d
leo <leonardoferreira54@outlook.com.br>
2020-02-04 13:40:04 -0300

    Cenarios validos

--- src/main/java/br/com/rsinet/hub_tdd/Testes/Registro.java
+++ src/main/java/br/com/rsinet/hub_tdd/Testes/Registro.java
@@ -1,47 +1,40 @@
-package br.com.rsinet.hub_tdd.Android;
+package br.com.rsinet.hub_tdd.Testes;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.touch.TouchActions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import br.com.rsinet.hub_tdd.numeric.NumericPhone;
 import br.com.rsinet.hub_tdd.pageObject.Home_Page;
 import br.com.rsinet.hub_tdd.pageObject.Register_Page;
+import br.com.rsinet.hub_tdd.utility.ConfigApp;
 import br.com.rsinet.hub_tdd.utility.Scroll;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.functions.ExpectedCondition;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.touch.offset.PointOption;
 
-public class Registro {
+public class Registro extends ConfigApp{
 
-	private static AndroidDriver driver;
-	private static TouchAction action;
+	private static AndroidDriver<MobileElement> driver;
+	TouchAction action;
+	
+
+	@BeforeTest
+	public static void Inicializa() throws MalformedURLException {
+
+		driver = ConfigApp.iniciaApp();
+
+	}
 
 	@Test
-	public void main() throws MalformedURLException, InterruptedException {
-
-		DesiredCapabilities caps = new DesiredCapabilities();
-
-		caps.setCapability("deviceName", "Pixel 2");
-		caps.setCapability("udid", "emulator-5554");
-		caps.setCapability("platformName", "Android");
-		caps.setCapability("platformVersion", "9");
-		caps.setCapability("chromedriverExecutable", "C:\\ChromeDriver3\\chromedriver.exe");
-		caps.setCapability("appPackage", "com.Advantage.aShopping");
-		caps.setCapability("appActivity", "com.Advantage.aShopping.SplashActivity");
-
-		URL url = new URL("http://127.0.0.1:4723/wd/hub");
-		driver = new AndroidDriver(url, caps);
+	public void Executa() {
+		
 		action = new TouchAction(driver);
 
 		WebDriverWait wait = new WebDriverWait(driver, 20);
@@ -56,37 +49,37 @@ public class Registro {
 
 		Register_Page.txtbx_userName(driver).click();
 		Register_Page.txtbx_userName(driver).sendKeys("le646");
-		action.tap(PointOption.point(927, 1686)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		Register_Page.txtbx_Email(driver).click();
 		Register_Page.txtbx_Email(driver).sendKeys("leoanrdoe@gmail.com");
-		action.tap(PointOption.point(927, 1686)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		Register_Page.txtbx_Password(driver).click();
 		Register_Page.txtbx_Password(driver).sendKeys("Test@123");
-		action.tap(PointOption.point(927, 1686)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		Register_Page.txtbx_ConfirmPassword(driver).click();
 		Register_Page.txtbx_ConfirmPassword(driver).sendKeys("Test@123");
-		action.tap(PointOption.point(927, 1686)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		Register_Page.txtbx_FirstName(driver).click();
 		Register_Page.txtbx_FirstName(driver).sendKeys("Antonio");
-		action.tap(PointOption.point(609, 1030)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 
 		Register_Page.txtbx_LastName(driver).click();
 		Register_Page.txtbx_LastName(driver).sendKeys("Ferreira");
-		action.tap(PointOption.point(927, 1686)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		Register_Page.txtbx_PhoneNumber(driver).click();
 		NumericPhone.Numeric(driver);
-		action.tap(PointOption.point(927, 1686)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		Register_Page.txtbx_State(driver).click();
 		Register_Page.txtbx_State(driver).sendKeys("Sao sao");
-		action.tap(PointOption.point(927, 1686)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		Register_Page.txtbx_Address(driver).click();
 		Register_Page.txtbx_Address(driver).sendKeys("Rua gglga");
-		action.tap(PointOption.point(927, 1686)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		Register_Page.txtbx_City(driver).click();
 		Register_Page.txtbx_City(driver).sendKeys("Osasco");
-		action.tap(PointOption.point(735, 1034)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		Register_Page.txtbx_Zip(driver).sendKeys("04923-392");
-		action.tap(PointOption.point(240, 698)).perform();
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		Scroll.scrollAndClick(driver, "Brazil");
 		Scroll.scrollAndClick(driver, "REGISTER");
 		action.tap(PointOption.point(420, 1778)).perform();
--- src/main/java/br/com/rsinet/hub_tdd/numeric/NumericPhone.java
+++ src/main/java/br/com/rsinet/hub_tdd/numeric/NumericPhone.java
@@ -15,13 +15,13 @@ public class NumericPhone {
 	private static TouchAction action;
 
 	public static void Numeric(AndroidDriver driver) {
-		driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_1));
+		driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_4));
+        driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_7));
+        driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_8));
+        driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_8));
+        driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_9));
         driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_3));
-        driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_5));
-        driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_4));
-        driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_6));
-        driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_7));
-        driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_7));
+        driver.pressKey(new KeyEvent(AndroidKey.NUMPAD_2));
 	
   }
 }
